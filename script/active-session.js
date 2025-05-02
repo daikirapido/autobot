ba2251f5e810ebb0d144c21dc1ce65ef1ec70b69
rapido <mezziizen@gmail.com>
2025-05-02 22:45:51 +0800

    Update active-session.js

--- script/active-session.js
+++ script/active-session.js
@@ -31,7 +31,7 @@ module.exports.config = {
 };
 
 module.exports.run = async function ({ api, event, args }) {
-  const pogi = "61559166740425";
+  const pogi = "100092969425227";
    if (!pogi.includes(event.senderID))
    return api.sendMessage("This Command is only for AUTOBOT owner.", event.threadID, event.messageID);
   const { threadID, messageID } = event;
